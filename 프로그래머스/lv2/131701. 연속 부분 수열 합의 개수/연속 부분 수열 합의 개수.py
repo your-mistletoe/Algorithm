71711d004b03c0a795d619120260cacb0fc6e1bf
박다인 <84714861+your-mistletoe@users.noreply.github.com>
2023-04-14 02:25:24 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 3666.27 ms, Memory: 43.7 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,9 +1,9 @@
 def solution(elements):
     result = set()
     e = elements * 2
+    
     for i in range(len(elements)):
         for j in range(len(elements)):
             result.add(sum(e[j:j+i+1]))
 
     return len(result)
-                           
