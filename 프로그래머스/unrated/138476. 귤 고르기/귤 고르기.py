1f109e4accc6457447ceae138b97038d7dc30700
박다인 <84714861+your-mistletoe@users.noreply.github.com>
2023-04-17 03:28:02 +0900

    [unrated] Title: 귤 고르기, Time: 44.83 ms, Memory: 21.8 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
@@ -1,17 +1,9 @@
 from collections import Counter
 def solution(k, tangerine):
-    dic = {}
-    q = []
     cnt = 0
 
-    # for i in tangerine:
-    #     if i in dic:
-    #         dic[i] += 1
-    #     else:
-    #         dic[i] = 1
-
     d = Counter(tangerine)
-    # sort_dic = sorted(dic.items(), key=lambda x:x[1], reverse=True)
+    
     sort_dic = sorted(list(d.items()), key=lambda x:x[1], reverse=True)
 
     for i, v in sort_dic:
@@ -20,6 +12,5 @@ def solution(k, tangerine):
         
         if k <= 0:
             break
-        
 
     return cnt
