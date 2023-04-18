ae88447133166f5e15329f10eee381e60f4b2c72
박다인 <84714861+your-mistletoe@users.noreply.github.com>
2023-04-19 04:06:51 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+def fibo(n):
+  a, b = 1, 1
+  if n == 0:
+    return 0
+  if n == 1 or n == 2:
+    return 1
+  for _ in range(1, n):
+    a, b = b, b + a
+  return a
+
+print(fibo(n))
