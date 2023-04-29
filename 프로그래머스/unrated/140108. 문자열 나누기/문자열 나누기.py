3064084dde34ec279682d8e8427824268f01b472
박다인 <84714861+your-mistletoe@users.noreply.github.com>
2023-04-30 01:31:27 +0900

    [unrated] Title: 문자열 나누기, Time: 2.52 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.py
@@ -0,0 +1,21 @@
+def solution(s):
+    pivot = 0
+    same = 0
+    dif = 0
+    arr = []
+    for i, v in enumerate(s):
+        if v == s[pivot]:
+            same += 1
+        else:
+            dif += 1
+            
+        if same == dif:
+            arr.append(s[pivot:i+1])
+            if i < len(s)-1:
+                pivot = i + 1
+        else:
+            if len(s) - i <  2:
+                arr.append(s[pivot:len(s)-1])
+        
+    print(arr)
+    return len(arr)
